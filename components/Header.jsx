db7d2876aa29b176fbd4e0b1bb30e34585359c50
Juan David García Rincón <juandagarciadev@gmail.com>
2024-10-26 15:24:29 -0500

    Alliances Menu Link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,93 +1,96 @@
 // app/components/Header.js
-"use client";
-import React, { useState, useEffect } from "react";
-import { IoMenu } from "react-icons/io5";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+'use client'
+import React, { useState, useEffect } from 'react'
+import { IoMenu } from 'react-icons/io5'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-  SheetClose,
-} from "@/components/ui/sheet";
-import { IoMdClose } from "react-icons/io";
+	Sheet,
+	SheetContent,
+	SheetTrigger,
+	SheetClose,
+} from '@/components/ui/sheet'
+import { IoMdClose } from 'react-icons/io'
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false)
 
-  useEffect(() => {
-    const handleScroll = () => setMenuOpen(false);
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+	useEffect(() => {
+		const handleScroll = () => setMenuOpen(false)
+		window.addEventListener('scroll', handleScroll)
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, [])
 
-  const handleMenuToggle = () => setMenuOpen(!menuOpen);
-  const handleMenuOptionClick = () => setMenuOpen(false);
+	const handleMenuToggle = () => setMenuOpen(!menuOpen)
+	const handleMenuOptionClick = () => setMenuOpen(false)
 
-  return (
-    <div className="fixed top-0 left-0 w-full z-30 py-9 bg-transparent">
-      <div className="flex items-center justify-between w-[90%] max-w-screen-xl mx-auto">
-        <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
-          <SheetTrigger asChild>
-            <button onClick={handleMenuToggle}>
-              <IoMenu color="#fff" size={50} />
-            </button>
-          </SheetTrigger>
-          <SheetContent
-            side="left"
-            className="bg-black/45 border-none px-9 py-32 md:px-12 lg:px-20"
-          >
-            <div className="absolute top-8 right-8">
-              <SheetClose className="text-white text-xl font-medium">
-                <IoMdClose />
-              </SheetClose>
-            </div>
-            <ul className="flex flex-col gap-6 font-medium text-xl text-white md:text-2xl">
-              <li onClick={handleMenuOptionClick}>
-                <Link href="/">INICIO</Link>
-              </li>
-              <li onClick={handleMenuOptionClick}>
-                <Link href="/habitaciones">HABITACIONES</Link>
-              </li>
-              <li onClick={handleMenuOptionClick}>
-                <Link href="/espacios">ESPACIOS</Link>
-              </li>
-              <li onClick={handleMenuOptionClick}>
-                <Link href="/contacto">CONTACTO</Link>
-              </li>
+	return (
+		<div className="fixed top-0 left-0 w-full z-30 py-9 bg-transparent">
+			<div className="flex items-center justify-between w-[90%] max-w-screen-xl mx-auto">
+				<Sheet open={menuOpen} onOpenChange={setMenuOpen}>
+					<SheetTrigger asChild>
+						<button onClick={handleMenuToggle}>
+							<IoMenu color="#fff" size={50} />
+						</button>
+					</SheetTrigger>
+					<SheetContent
+						side="left"
+						className="bg-black/45 border-none px-9 py-32 md:px-12 lg:px-20"
+					>
+						<div className="absolute top-8 right-8">
+							<SheetClose className="text-white text-xl font-medium">
+								<IoMdClose />
+							</SheetClose>
+						</div>
+						<ul className="flex flex-col gap-6 font-medium text-xl text-white md:text-2xl">
+							<li onClick={handleMenuOptionClick}>
+								<Link href="/">INICIO</Link>
+							</li>
+							<li onClick={handleMenuOptionClick}>
+								<Link href="/habitaciones">HABITACIONES</Link>
+							</li>
+							<li onClick={handleMenuOptionClick}>
+								<Link href="/espacios">ESPACIOS</Link>
+							</li>
+							<li onClick={handleMenuOptionClick}>
+								<Link href="/alliances">ALIADOS</Link>
+							</li>
+							<li onClick={handleMenuOptionClick}>
+								<Link href="/contacto">CONTACTO</Link>
+							</li>
 
-              {/* PARA ACTIVAR EXPERIENCIAS EN EL MENÚ QUITAR EL HIDDEN */}
-              <li onClick={handleMenuOptionClick} className="hidden">
-                <Link href="/experiencias">EXPERIENCIAS</Link>
-              </li>
-            </ul>
-          </SheetContent>
-        </Sheet>
+							{/* PARA ACTIVAR EXPERIENCIAS EN EL MENÚ QUITAR EL HIDDEN */}
+							<li onClick={handleMenuOptionClick} className="hidden">
+								<Link href="/experiencias">EXPERIENCIAS</Link>
+							</li>
+						</ul>
+					</SheetContent>
+				</Sheet>
 
-        <div className="hidden sm:flex justify-center flex-grow pl-[101px]">
-          <Link href="/">
-            <Image
-              src="/icons/heiss.svg"
-              alt="Heiss logo"
-              width={120}
-              height={30}
-              priority
-            />
-          </Link>
-        </div>
+				<div className="hidden sm:flex justify-center flex-grow pl-[101px]">
+					<Link href="/">
+						<Image
+							src="/icons/heiss.svg"
+							alt="Heiss logo"
+							width={120}
+							height={30}
+							priority
+						/>
+					</Link>
+				</div>
 
-        <Button variant="outline" asChild className="text-white">
-          <Link
-            href="https://hotels.cloudbeds.com/es/reservation/lLxxdq"
-            target="_blank"
-          >
-            RESERVE YA
-          </Link>
-        </Button>
-      </div>
-    </div>
-  );
+				<Button variant="outline" asChild className="text-white">
+					<Link
+						href="https://hotels.cloudbeds.com/es/reservation/lLxxdq"
+						target="_blank"
+					>
+						RESERVE YA
+					</Link>
+				</Button>
+			</div>
+		</div>
+	)
 }
 
-export default Header;
+export default Header
